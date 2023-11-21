b7c8de6039c0490e08e671bd8614af37551dd2c5
priyamkundu01 <priyamkundu4411@gmail.com>
2023-11-22 03:48:22 +0530

    add html, css, and js file

---
+++ js/app.js
@@ -0,0 +1,46 @@
+function saveData() {
+    const noValue = document.getElementById('noInput').value;
+    const capacityValue = document.getElementById('capacityInput').value;
+
+    // Validate input (you can add more sophisticated validation)
+    if (!noValue || !capacityValue) {
+        alert('Please fill in both fields.');
+        return;
+    }
+
+    // Create a data object
+    const data = {
+        no: parseInt(noValue),
+        capacity: parseInt(capacityValue),
+    };
+
+    // Send data to the backend API (you need to implement the backend)
+    sendDataToBackend(data);
+}
+
+function sendDataToBackend(data) {
+    // Assuming you have a backend API endpoint (replace URL accordingly)
+    const apiUrl = 'https://garbage-collect-backend.onrender.com/send-vehicle';
+
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(responseData => {
+        alert('Data saved successfully!');
+        // You can perform additional actions after successful save
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        alert('Failed to save data. Please try again.');
+    });
+}
